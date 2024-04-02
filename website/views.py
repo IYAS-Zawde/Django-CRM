9ec4d01da72eb69c48e7d691d933e5b6186cf163
Iyas Zawde <iyas.zawde@gmail.com>
2024-04-03 02:06:05 +0300

    Authentication
    Add login and logout functions to the website app, figure out more about blocks in Django, and build-in authentication system

--- website/views.py
+++ website/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html',{})
+    #check if requset method is POST or GET
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+
+        #Authonticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error")
+            return redirect('home')
+    else:
+        return render(request, 'home.html',{})
+
+# def login_user(request):
+#    pass
+
+def logout_user(request):
+    pass
