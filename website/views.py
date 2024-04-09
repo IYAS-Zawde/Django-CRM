6faf7a7240738922c458d9057d6158d91be955dc
Iyas Zawde <iyas.zawde@gmail.com>
2024-04-09 20:54:51 +0300

    Add new model (Record Model)

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     #check if requset method is POST or GET
@@ -27,3 +28,41 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You've been logged out successfully")
     return redirect('home')
+
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+def register_user_2(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authonticate and loging
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username= username, password = password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered Well Done!!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form':form})
+        
+    return render(request, 'register.html', {'form':form})
