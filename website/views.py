1f9815625af4e80fdaee71ceed77c9018edb3738
Iyas Zawde <iyas.zawde@gmail.com>
2024-04-09 21:04:01 +0300

    View records

--- website/views.py
+++ website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     #check if requset method is POST or GET
     if request.method == 'POST':
         username = request.POST['user_name']
@@ -19,7 +22,7 @@ def home(request):
             messages.success(request, "There was an error")
             return redirect('home')
     else:
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records':records})
 
 # def login_user(request):
 #    pass
