46ca7de6651693ae1d4ec139028a804655b92665
Iyas Zawde <iyas.zawde@gmail.com>
2024-04-13 23:16:44 +0300

    Record Card
    Record card for each customer in a specific page, with delete and edit buttons (without implementing there functionality)

--- website/views.py
+++ website/views.py
@@ -69,3 +69,14 @@ def register_user_2(request):
         return render(request, 'register.html',{'form':form})
         
     return render(request, 'register.html', {'form':form})
+
+def cutomer_record (request, pk):
+     if request.user.is_authenticated:
+          #Look Up Records
+          customer_record = Record.objects.get(id=pk)
+          return render(request, 'record.html',{'customer_record':customer_record})
+     else:
+        messages.success(request, "You must be logged in in order to access this webpage ...")
+        return redirect('home') 
+
+     pass
